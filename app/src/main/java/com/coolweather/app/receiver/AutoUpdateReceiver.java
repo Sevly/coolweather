f88d211f432e69141ed7cda2afd3062672d90a9f
Sevly <490873520@qq.com>
2016-12-20 14:10:27 +0800

    Finished.

---
+++ app/src/main/java/com/coolweather/app/receiver/AutoUpdateReceiver.java
@@ -0,0 +1,21 @@
+package com.coolweather.app.receiver;
+
+/**
+ * Created by Sevly on 2016/12/20.
+ */
+
+import com.coolweather.app.service.AutoUpdateService;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AutoUpdateReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent i = new Intent(context, AutoUpdateService.class);
+        context.startService(i);
+    }
+
+}
